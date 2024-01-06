281858b657ee2f9d5ca8012c463069d0163bbaa8
warren2k <846021+warren2k@users.noreply.github.com>
2024-01-06 09:45:33 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/eelco/src/expression.rs

--- crates/eelco/src/expression.rs
+++ crates/eelco/src/expression.rs
@@ -1,3 +1,5 @@
+use crate::{examples::Example, example_id::ExampleId};
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct ExpressionExample {
 
@@ -5,6 +7,7 @@ pub(crate) struct ExpressionExample {
 
 impl ExpressionExample {
     pub(crate) fn new(path: camino::Utf8PathBuf, line: usize, expression: String) -> Self {
-        todo!()
+        let id = ExampleId::new(path, line);
+        Self { id, expression }
     }
 }
