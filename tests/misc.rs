7706181d96f7b40f26191e9a8e8531e3d02035e9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-02-11 20:54:18 +0700

    test: test all_examples_tested
    Co-authored-by: furioncycle <ttecho2021@gmail.com>
    Co-authored-by: warren2k <846021+warren2k@users.noreply.github.com>

--- tests/misc.rs
+++ tests/misc.rs
@@ -1,5 +1,7 @@
 mod util;
 
+use assert_fs::fixture::FileWriteStr;
+use indoc::{formatdoc, indoc};
 use util::with_eelco;
 
 #[test]
@@ -9,5 +11,32 @@ fn empty_file() {
             .assert()
             .failure()
             .stderr("Error: could not find any REPL examples\n");
+    });
+}
+
+#[test]
+fn all_examples_tested() {
+    with_eelco(|file, eelco| {
+        file.write_str(indoc! {"
+                ```nix-repl
+                nix-repl> 1 + 1
+
+                2
+                ```
+
+                ```nix-repl
+                nix-repl> 1 + 2
+
+                3
+                ```
+            "})
+            .unwrap();
+
+        let file_path = file.path().to_str().unwrap();
+
+        eelco.assert().success().stderr(formatdoc! {"
+            PASS: {file_path}:1
+            PASS: {file_path}:7
+        "});
     });
 }
