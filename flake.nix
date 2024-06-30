1769b42d8bfa8908df3b23d105597f2003346ca6
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-06-30 21:40:52 +0700

    chore: rm relic comment

--- flake.nix
+++ flake.nix
@@ -62,8 +62,7 @@
         // {
           inherit cargoArtifacts NIX_CMD_PATH NIX_INSTANTIATE_CMD_PATH;
           nativeCheckInputs = [pkgs.nix];
-          # 1. integration tests execute `nix`, which fails creating `/nix/var`
-          # 2. integration tests require `/dev/ptmx`
+          # integration tests execute `nix`, which fails creating `/nix/var`
           doCheck = false;
         }
       );
